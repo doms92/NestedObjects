fe30cd8c645a32c1c3e803689b058db63c14cfbc
doms92 <summers8734@student.cptc.edu>
2021-01-18 11:36:46 -0800

    Added Instructor Class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// The legal full name. First and Last
+        /// Example: Jane Doe
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Company email address 
+        /// Example: Jane.Doe@cptc.edu
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
