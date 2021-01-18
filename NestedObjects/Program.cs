afe951597f76f64c507ea2d4136bce2aa01f919e
doms92 <summers8734@student.cptc.edu>
2021-01-18 12:25:18 -0800

    Added Test Code

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -10,6 +10,51 @@ namespace NestedObjects
     {
         static void Main(string[] args)
         {
+            Instructor currInst = new Instructor()
+            {
+                FullName = "Homer Simpson",
+                Email = "Homer.Simpson@cptc.edu"
+            };
+
+            Student s1 = new Student()
+            {
+                FirstName = "Jim",
+                LastName = "Halpert"
+            };
+
+            Student s2 = new Student()
+            {
+                FirstName = "Creed",
+                LastName = "Bratton"
+            };
+
+            Student s3 = new Student()
+            {
+                FirstName = "Kelly",
+                LastName = "Brown"
+            };
+
+            Course testCourse = new Course();
+            testCourse.Title = "CPW 212 - Adv .NET";
+            testCourse.Credits = 5;
+            testCourse.CourseInstructor = currInst;
+            testCourse.Roster = new List<Student>();
+
+            // Adding students to course
+            testCourse.Roster.Add(s1);
+            testCourse.Roster.Add(s2);
+            testCourse.Roster.Add(s3);
+
+            Console.WriteLine(testCourse.Title);
+            Console.WriteLine("Is taught by " +
+                testCourse.CourseInstructor.FullName);
+
+            Console.WriteLine("Currently Enrolled Students: ");
+            foreach(Student s in testCourse.Roster)
+            {
+                Console.WriteLine($"{s.FirstName} {s.LastName}");
+            }
+            Console.ReadKey();
         }
     }
 }
